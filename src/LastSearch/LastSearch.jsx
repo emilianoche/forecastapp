68a855b6271ff446d483e8f8e90b67d402c9e1e9
emilianoche <chequeremiliano@gmail.com>
2018-10-30 15:50:53 -0300

    solution of warn

--- src/LastSearch/LastSearch.jsx
+++ src/LastSearch/LastSearch.jsx
@@ -6,10 +6,10 @@ export default class LastSearch extends Component {
     return(
       <footer className="searches">
         <ul className='list'>
-          <h3 class="pink">Last Searchs</h3>
+          <h3 className="pink">Last Searchs</h3>
           {this.props.searchs.map(city => {
             return (
-              <li><button onClick={()=>this.props.onClickCity(city)}>{city.name},{city['sys'].country}</button><button onClick={() => this.props.deleteSearch(city)}>X</button></li>
+              <li key={city.id}><button onClick={()=>this.props.onClickCity(city)}>{city.name},{city['sys'].country}</button><button onClick={() => this.props.deleteSearch(city)}>X</button></li>
             )
           })}
         </ul>
